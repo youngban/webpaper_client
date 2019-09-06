9ba7a2fc4710fcb81f86eb008fecdcf757ba50b3
youngban <ban1990@naver.com>
2019-09-06 21:34:17 +0900

    when click articles axios post count

--- src/components/TopicInfo.js
+++ src/components/TopicInfo.js
@@ -1,11 +1,10 @@
 import React from 'react';
 // import fakeData from '../fakeData';
 
-const TopicInfo = ({ match }) => {
+const TopicInfo = props => {
   return (
     <div>
       <h2>test</h2>
-      <h3>{console.log(match)}</h3>
     </div>
   );
 };
