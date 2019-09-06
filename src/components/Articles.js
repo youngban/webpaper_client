d389afc3f566c991ead95f70274afb9ca8d788ff
ggsuccess <ggsuccess@naver.com>
2019-09-06 21:22:21 +0900

    category,articles complete

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import './articles.css';
 const Articles = props => {
-  console.log(props.category.length);
+  //console.log(props.category);
   return (
     <div>
-      {props.data.map(item => (
-        <a key={item.id} item={item} href={item.url} className="articles">
-          {console.log(item.category)}
-          {props.category.length === 7
-            ? item.name
-            : item.category.includes(props.category)
-            ? item.name //지금 카테고리가 1개일때만 해당 카테고리 기사가 나오고 2개이상일땐 나오지않음
-            : ''}
-        </a>
-      ))}
+      {props.data.map(item => {
+        if (props.category.includes(item.category)) {
+          return (
+            <a href={item.url} key={item.id} data={item} className="articles">
+              {item.name}
+            </a>
+          );
+        }
+        if (props.category.length === 0) {
+          return (
+            <a href={item.url} key={item.id} data={item} className="articles">
+              {item.name}
+            </a>
+          );
+        }
+      })}
     </div>
   );
 };
