83956cadb94b6e998a6eb87bba42e7e8c03a2b40
ggsuccess <ggsuccess@naver.com>
2019-09-10 12:56:45 +0900

    signUp complete,signin ing..

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -31,7 +31,11 @@ class SignUp extends Component {
           email: this.state.email,
           pw: this.state.pw
         })
-        .then(res => console.log(res))
+        .then(res =>
+          res.status === 200
+            ? alert('회원가입이 완료되었습니다')
+            : alert('잘못된 요청으로 회원가입이 되지않았습니다.')
+        )
         .then(this.props.history.push('/'))
         .catch(err => console.log('signuperr:' + err));
     }
